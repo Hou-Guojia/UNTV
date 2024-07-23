d5e4455cd08e4406a9856b51f07592072b7e2a68
lin-zukai <lzukai@163.com>
2024-07-23 10:53:10 +0800

    Update Demo.m

--- UNTV-Code/Demo.m
+++ UNTV-Code/Demo.m
@@ -1,12 +1,9 @@
 %% 
-% Matlab demo code for "A Variational Framework for Underwater Image
-%Dehazing and Deblurring"
-% 
-% created by Jun Xie (my_love_mj@126.com) and Goujia Hou (hgjouc@126.com)
-% 
-% Copyright @ Jun Xie and Guojia Hou, 2020. 
-%This software is free for academic usage. 
-%If you publish results obtained using this software, please cite our paper.
+% Code for "A Variational Framework for Underwater Image Dehazing and Deblurring"
+% Jun Xie, Guojia Hou*, Guodong Wang, Zhenkuan Pan
+% IEEE Transactions on Circuits and Systems for Video Technology, vol. 32, no.6, 2022
+% https://doi.org/10.1109/TCSVT.2021.3115791
+%This code is free for academic usage. 
 %%
 %All the experiments are implemented in MATLAB 2016b on an Intel 3.2GHz PC with 8GB RAM.
 %This code runs on several versions Matlab included 2014/2016/2018/2019/2020
